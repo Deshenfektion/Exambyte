60022f155bfbdd183fef3b949392dbea36a1ca1c
Onursonn <97085413+Onursonn@users.noreply.github.com>
2024-11-15 20:38:10 +0100

    Add files via upload

---
+++ src/test/java/de/hhu/exambyte/ExambyteApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hhu.exambyte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExambyteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
