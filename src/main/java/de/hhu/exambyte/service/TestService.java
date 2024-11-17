6ae9660d1c5d42c8d70bb98db621e4222c5a72fc
deshenrao <deshen.rao@immofolia.com>
2024-11-17 17:37:33 +0100

    Create dummy test service

--- src/main/java/de/hhu/exambyte/service/TestService.java
+++ src/main/java/de/hhu/exambyte/service/TestService.java
@@ -1,4 +1,25 @@
 package de.hhu.exambyte.service;
 
+import de.hhu.exambyte.model.Test;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class TestService {
+
+    public List<Test> getAvailableTests() {
+        Test test1 = new Test("Test1", 01);
+        Test test2 = new Test("Test2", 02);
+        Test test3 = new Test("Test3", 03);
+        List<Test> tests = new ArrayList<>();
+        tests.add(test1);
+        tests.add(test2);
+        tests.add(test3);
+        return tests;
+    }
+
+/*    public Test createTest() {
+    }*/
 }
