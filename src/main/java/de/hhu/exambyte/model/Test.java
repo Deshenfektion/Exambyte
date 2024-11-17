6732b4c16ce4cc3669da38896c2ff572eba33167
deshenrao <deshen.rao@immofolia.com>
2024-11-17 17:37:10 +0100

    Create basic dummy test model

---
+++ src/main/java/de/hhu/exambyte/model/Test.java
@@ -0,0 +1,15 @@
+package de.hhu.exambyte.model;
+
+public class Test {
+    private String name;
+    private int id;
+
+    public Test(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+}
