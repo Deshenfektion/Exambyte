871944302165d3790d2381f38bad4a35e91c755d
deshenrao <deshen.rao@immofolia.com>
2024-11-17 17:40:43 +0100

    Create student controller with get mapping for student dashboard

--- src/main/java/de/hhu/exambyte/controller/StudentController.java
+++ src/main/java/de/hhu/exambyte/controller/StudentController.java
@@ -1,13 +1,30 @@
 package de.hhu.exambyte.controller;
 
+import de.hhu.exambyte.model.Test;
+import de.hhu.exambyte.service.TestService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 @Controller
+@RequestMapping("/student")
 public class StudentController {
-    @GetMapping("/student")
-    public String getStudentHomepage() {
-        return "student-homepage";
+
+    @Autowired
+    private TestService testService;
+
+    @GetMapping("")
+    public String showStudentDashboard(Model model) {
+        List<Test> availableTests = testService.getAvailableTests();
+        model.addAttribute("tests", availableTests);
+        return "student-dashboard";
     }
 
+/*    public String showNewTest() {
+
+    }*/
 }
