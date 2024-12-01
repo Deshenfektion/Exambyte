1d8b2a020560593edd7e7d4402b84a474c724371
SahneJoghurt <ruthkng33@gmail.com>
2024-12-01 16:00:44 +0100

    OAuth2 Rollenbasierter Zugriff auf Controller gesichert

--- src/main/java/de/hhu/exambyte/controller/CorrectorController.java
+++ src/main/java/de/hhu/exambyte/controller/CorrectorController.java
@@ -1,4 +1,22 @@
 package de.hhu.exambyte.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/corrector")
 public class CorrectorController {
+
+    @GetMapping("")
+    @Secured("ROLE_CORRECTOR")
+    public String indexCorrector(OAuth2AuthenticationToken auth, Model m) {
+        String login = auth.getPrincipal().getAttribute("login");
+        System.out.println(auth);
+        m.addAttribute("name", login);
+        return "corrector-dashboard";
+    }
 }
--- src/main/java/de/hhu/exambyte/controller/StudentController.java
+++ src/main/java/de/hhu/exambyte/controller/StudentController.java
@@ -3,6 +3,8 @@ package de.hhu.exambyte.controller;
 import de.hhu.exambyte.model.Test;
 import de.hhu.exambyte.service.TestService;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,6 +22,12 @@ public class StudentController {
 
     @GetMapping("")
     @Secured("ROLE_STUDENT")
+    public String indexStudent(OAuth2AuthenticationToken auth, Model m) {
+        String login = auth.getPrincipal().getAttribute("login");
+        System.out.println(auth);
+        m.addAttribute("name", login);
+        return "student-dashboard";
+    }
     public String showStudentDashboard(Model model) {
         List<Test> availableTests = testService.getAvailableTests();
         model.addAttribute("tests", availableTests);
