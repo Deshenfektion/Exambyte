ff58b5f12b7dbce773e96e88d84e0bf180e9be60
SahneJoghurt <ruthkng33@gmail.com>
2024-12-18 17:46:58 +0100

    OrganizerController & templates

--- src/main/java/de/hhu/exambyte/controller/OrganizerController.java
+++ src/main/java/de/hhu/exambyte/controller/OrganizerController.java
@@ -8,13 +8,9 @@ import org.springframework.security.oauth2.client.authentication.OAuth2Authentic
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import de.hhu.exambyte.domain.model.Test;
-import de.hhu.exambyte.application.service.TestService;
 
 @Controller
 @RequestMapping("/organizer")
@@ -33,31 +29,31 @@ public class OrganizerController {
         List<Test> allTests = testService.getTestsForOrganizers();
         m.addAttribute("tests", allTests);
 
-        return "organizer-dashboard";
+        return "organizer/dashboard";
     }
 
     @GetMapping("/new-test")
     @Secured("ROLE_ORGANIZER")
     public String newTest() {
-        return "new_test";
+        return "organizer/new-test";
     }
-
-    @PostMapping("/new-test")
-    @Secured("ROLE_ORGANIZER")
-    public String createNewTest(@RequestParam String choice) {
-        if ("ja".equalsIgnoreCase(choice)) {
-            return "redirect:/organizer/create-test";
-        } else {
-            return "redirect:/organizer";
-        }
-    }
-
-    @GetMapping("/test/{id}")
-    @Secured("ROLE_ORGANIZER")
-    public String viewTest(@PathVariable String id, Model model) {
-        Test test = testService.getTestById(id);
-        model.addAttribute("test", test);
-        return "test-details";
-    }
+//
+//    @PostMapping("/new-test")
+//    @Secured("ROLE_ORGANIZER")
+//    public String createNewTest(@RequestParam String choice) {
+//        if ("ja".equalsIgnoreCase(choice)) {
+//            return "redirect:/organizer/create-test";
+//        } else {
+//            return "redirect:/organizer";
+//        }
+//    }
+//
+//    @GetMapping("/test/{id}")
+//    @Secured("ROLE_ORGANIZER")
+//    public String viewTest(@PathVariable String id, Model model) {
+//        Test test = testService.getTestById(id);
+//        model.addAttribute("test", test);
+//        return "test-details";
+//    }
 
 }
