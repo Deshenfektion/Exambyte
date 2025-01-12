4db8717b4bebdd34ae55ed2b57e83672a0f7b006
SahneJoghurt <ruthkng33@gmail.com>
2025-01-12 16:09:49 +0100

    OAuth2 authorization tests completed

--- src/test/java/de/hhu/exambyte/controller/OrganizerControllerTest.java
+++ src/test/java/de/hhu/exambyte/controller/OrganizerControllerTest.java
@@ -1,28 +1,45 @@
 package de.hhu.exambyte.controller;
 
+import de.hhu.exambyte.application.service.TestService;
+import de.hhu.exambyte.configuration.MethodSecurity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import de.hhu.exambyte.controller.OrganizerController;
 import de.hhu.exambyte.helper.WithMockOAuth2User;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(OrganizerController.class)
+@Import({MethodSecurity.class})
 public class OrganizerControllerTest {
+
     @Autowired
     MockMvc mockMvc;
 
+    @MockBean
+    private TestService testService;
+
     @Test
     @WithMockOAuth2User(roles = "ORGANIZER")
-    @DisplayName("Organizer Dashboard ist erreichbar unter /organizer")
-    void test1() throws Exception {
-        mockMvc.perform(get("/organizer")).andExpect(status().isOk());
+    @DisplayName("Organizer kann sich anmelden und Dashboard ist erreichbar")
+    void testOrganizerAccess() throws Exception {
+        mockMvc.perform(get("/organizer"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("organizer/dashboard"));
+    }
+
+    @Test
+    @WithMockOAuth2User(roles = "USER")
+    @DisplayName("Normale Benutzer können nicht auf das Organizer-Dashboard zugreifen")
+    void testUnauthorizedUserAccess() throws Exception {
+        mockMvc.perform(get("/organizer"))
+                .andExpect(status().isForbidden());
     }
 }
