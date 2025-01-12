d9548c64392e56446915a3d77fb3a57414022136
deshenrao <derao101@uni-duesseldorf.de>
2025-01-12 17:18:25 +0100

    Add missing @Repository annotation

--- src/main/java/de/hhu/exambyte/infrastructure/persistence/repository/QuestionRepository.java
+++ src/main/java/de/hhu/exambyte/infrastructure/persistence/repository/QuestionRepository.java
@@ -1,9 +1,11 @@
 package de.hhu.exambyte.infrastructure.persistence.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import de.hhu.exambyte.domain.model.Question;
 
+@Repository
 public interface QuestionRepository extends CrudRepository<Question, String> {
 
 }
--- src/main/java/de/hhu/exambyte/infrastructure/persistence/repository/TestRepository.java
+++ src/main/java/de/hhu/exambyte/infrastructure/persistence/repository/TestRepository.java
@@ -1,9 +1,11 @@
 package de.hhu.exambyte.infrastructure.persistence.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import de.hhu.exambyte.domain.model.Test;
 
+@Repository
 public interface TestRepository extends CrudRepository<Test, String> {
 
 }
