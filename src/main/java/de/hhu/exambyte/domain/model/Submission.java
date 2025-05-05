1eff12f7752c552149f32d1dcdb3fa1a2fd3a6ff
Deshenfektion <deshen.r1502@gmail.com>
2025-05-05 16:36:28 +0200

    Update submission domain model to reference MC response options

--- src/main/java/de/hhu/exambyte/domain/model/Submission.java
+++ src/main/java/de/hhu/exambyte/domain/model/Submission.java
@@ -2,41 +2,64 @@ package de.hhu.exambyte.domain.model;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.Collections; // Import für leeres Set
+import java.util.Set; // Import für Set
 
 /**
  * Repräsentiert die Einreichung eines Studierenden für eine einzelne Frage
  * eines Tests.
  * Behandelt als eigener Aggregate Root.
  */
-@Table("submissions") // Map record to the 'submissions' table
+@Table("submissions")
 public record Submission(
-        @Id Long id, // Primary Key
-
-        // Referenz zum Test-Aggregat (speichert nur die Test-ID)
+        @Id Long id,
         AggregateReference<Test, Long> testId,
-
-        // Referenz zum Question-Aggregat (speichert nur die Question-ID)
         AggregateReference<Question, Long> questionId,
+        String studentGithubId,
+        String submittedText, // Für Freitext
 
-        String studentGithubId, // Eindeutige ID des Studierenden (z.B. 'github|123456')
-        String submittedText, // Die Freitext-Antwort des Studierenden (null bei MC)
-        // TODO: Wie MC-Antworten speichern? Evtl. Set<AggregateReference<AnswerOption,
-        // Long>> selectedOptions;
+        // NEU: Set von Referenzen auf ausgewählte Antwortoptionen (für MC)
+        // Bezieht sich auf die Zwischentabelle 'submission_selected_options'.
+        // 'submission_id' ist die Spalte in der Zwischentabelle, die auf diese
+        // Submission verweist.
+        // 'answer_option_id' ist die Spalte in der Zwischentabelle, die auf die
+        // AnswerOption verweist.
+        @MappedCollection(idColumn = "submission_id", keyColumn = "answer_option_id") Set<AggregateReference<AnswerOption, Long>> selectedOptions,
 
-        // Bewertungsdetails (werden später von Korrektoren/System gefüllt)
-        Double score, // Erreichte Punkte (null, wenn noch nicht bewertet)
-        String feedback // Feedback des Korrektors (null, wenn nicht vorhanden)
-) {
-    // Konstruktor für neue, unbewertete Einreichungen ohne ID
+        Double score,
+        String feedback) {
+    // Konstruktor für neue, unbewertete Freitext-Einreichungen
     public Submission(AggregateReference<Test, Long> testId, AggregateReference<Question, Long> questionId,
             String studentGithubId, String submittedText) {
-        this(null, testId, questionId, studentGithubId, submittedText, null, null);
+        this(null, testId, questionId, studentGithubId, submittedText, Collections.emptySet(), null, null);
     }
 
-    // Methode zum Erstellen einer bewerteten Kopie (da Records immutable sind)
+    // Konstruktor für neue, unbewertete MC-Einreichungen
+    public Submission(AggregateReference<Test, Long> testId, AggregateReference<Question, Long> questionId,
+            String studentGithubId, Set<AggregateReference<AnswerOption, Long>> selectedOptions) {
+        this(null, testId, questionId, studentGithubId, null, selectedOptions, null, null);
+    }
+
+    // Methode zum Erstellen einer bewerteten Kopie
     public Submission withGrade(double score, String feedback) {
-        return new Submission(this.id, this.testId, this.questionId, this.studentGithubId, this.submittedText, score,
-                feedback);
+        // Behält die ursprünglichen Antworten (Text ODER Optionen) bei
+        return new Submission(this.id, this.testId, this.questionId, this.studentGithubId, this.submittedText,
+                this.selectedOptions, score, feedback);
+    }
+
+    // Methode zum Aktualisieren der Antworten (z.B. wenn Student erneut speichert)
+    // Für Freitext:
+    public Submission updateFreeText(String newText) {
+        return new Submission(this.id, this.testId, this.questionId, this.studentGithubId, newText,
+                this.selectedOptions, this.score, this.feedback);
+    }
+
+    // Für MC:
+    public Submission updateSelectedOptions(Set<AggregateReference<AnswerOption, Long>> newOptions) {
+        return new Submission(this.id, this.testId, this.questionId, this.studentGithubId, this.submittedText,
+                newOptions, this.score, this.feedback);
     }
 }
